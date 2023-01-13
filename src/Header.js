7805b69247d89297e4892fbbd85e2383201b50c0
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-13 19:34:53 +0530

    basic code added for explaination

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,18 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      Bunk<span className="yelhub">Hub</span>
+      <p className="title">
+        Bunk<span className="yelhub">Hub</span>
+        <br></br>
+        <br></br>
+        <span className="discription">
+          We are here to find you a room to relax
+        </span>
+        <br></br>
+        <span className="discription">
+          Please Select Time Slot
+        </span>
+      </p>
     </div>
   );
 }
