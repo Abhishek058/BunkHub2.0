e144b6e2645ce91b64e473cdf25cffe078d2afa6
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-14 23:20:25 +0530

    display rooms showing

--- src/Display.js
+++ src/Display.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Display() {
+  const rooms = [21, 33, 42, 57, 54];
   return (
-    <div className='display'>
-        <div className="title"> Available Rooms</div>
-        
+    <div className="display">
+      <div className="title"> Available Rooms</div>
+      <div className="romsdel">
+        {rooms.map((data) => (
+          <h4>{data}</h4>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Display
+export default Display;
