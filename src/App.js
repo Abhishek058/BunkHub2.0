5a6838c1a8895daf2d2480bba4602023a0fe0fbc
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-17 23:23:59 +0530

    display file deleted and adding it in timeslot file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Display from "./Display";
 import Header from "./Header.js";
 import TimeSlot from "./TimeSlot";
 
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <Header />
       <TimeSlot />
-      <Display />
     </div>
   );
 }
--- src/TimeSlot.js
+++ src/TimeSlot.js
@@ -1,23 +1,73 @@
 import React from "react";
 
 function TimeSlot() {
+  const rooms = [21, 33, 42, 57, 54];
+  const time1 = () => {
+    console.log("btn click");
+  };
   return (
     <div className="timeSlot">
       <div className="slotDiv1">
-        <div className="time">9:00<br/>-<br/>9:45</div>
-        <div className="time">9:45<br/>-<br/>10:30</div>
-        <div className="time">10:30<br/>-<br/>11:15</div>
-        <div className="time">11:15<br/>-<br/>12:00</div>
+        <div className="time" onClick={time1}>
+          9:00
+          <br />-<br />
+          9:45
+        </div>
+        <div className="time" onClick={time1}>
+          9:45
+          <br />-<br />
+          10:30
+        </div>
+        <div className="time" onClick={time1}>
+          10:30
+          <br />-<br />
+          11:15
+        </div>
+        <div className="time" onClick={time1}>
+          11:15
+          <br />-<br />
+          12:00
+        </div>
       </div>
       <div className="slotDiv2">
-        <div className="time">12:00<br/>-<br/>12:45</div>
-        <div className="time">12:45<br/>-<br/>13:30</div>
-        <div className="time">13:30<br/>-<br/>14:15</div>
-        <div className="time">14:15<br/>-<br/>15:00</div>
+        <div className="time" onClick={time1}>
+          12:00
+          <br />-<br />
+          12:45
+        </div>
+        <div className="time" onClick={time1}>
+          12:45
+          <br />-<br />
+          13:30
+        </div>
+        <div className="time" onClick={time1}>
+          13:30
+          <br />-<br />
+          14:15
+        </div>
+        <div className="time" onClick={time1}>
+          14:15
+          <br />-<br />
+          15:00
+        </div>
       </div>
       <div className="slotDiv3">
-        <div className="time">15:00<br/>-<br/>15:45</div>
-        <div className="time">15:45<br/>-<br/>16:30</div>
+        <div className="time" onClick={time1}>
+          15:00
+          <br />-<br />
+          15:45
+        </div>
+        <div className="time" onClick={time1}>
+          15:45
+          <br />-<br />
+          16:30
+        </div>
+      </div>
+      <div className="title"> Available Rooms</div>
+      <div className="romsdel">
+        {rooms.map((data) => (
+          <h4>{data}</h4>
+        ))}
       </div>
     </div>
   );
