e5e2479bdbce4a349229dcca62942cfdc7e34bbb
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-18 13:38:04 +0530

    json file data feted correctly

--- src/TimeSlot.js
+++ src/TimeSlot.js
@@ -1,81 +1,40 @@
 import React from "react";
+import data from "./time.json";
 
-const appi = './time.json'
+const btnDiv1 = ["9:00-9:45", "9:45-10:30", "10:30-11:15", "11:15-12:00"];
+const btnDiv2 = ["12:00-9:45", "12:45-13:30", "13:30-14:15", "14:15-15:00"];
+const btnDiv3 = ["15:00-15:45", "15:45-16:30"];
 
-const getTime=async()=>{
-  const response = await fetch(appi);
-  console.log(response);
-}
+const getTime = async (n) => {
+  console.log(data.data[n].avlRooms);
+};
 
 function TimeSlot() {
-  const rooms = [21, 33, 42, 57, 54];
-  const time1 = () => {
-    getTime();
-  };
   return (
     <div className="timeSlot">
       <div className="slotDiv1">
-        <div className="time" onClick={time1}>
-          9:00
-          <br />-<br />
-          9:45
-        </div>
-        <div className="time" onClick={time1}>
-          9:45
-          <br />-<br />
-          10:30
-        </div>
-        <div className="time" onClick={time1}>
-          10:30
-          <br />-<br />
-          11:15
-        </div>
-        <div className="time" onClick={time1}>
-          11:15
-          <br />-<br />
-          12:00
-        </div>
-      </div>
-      <div className="slotDiv2">
-        <div className="time" onClick={time1}>
-          12:00
-          <br />-<br />
-          12:45
-        </div>
-        <div className="time" onClick={time1}>
-          12:45
-          <br />-<br />
-          13:30
-        </div>
-        <div className="time" onClick={time1}>
-          13:30
-          <br />-<br />
-          14:15
-        </div>
-        <div className="time" onClick={time1}>
-          14:15
-          <br />-<br />
-          15:00
-        </div>
-      </div>
-      <div className="slotDiv3">
-        <div className="time" onClick={time1}>
-          15:00
-          <br />-<br />
-          15:45
-        </div>
-        <div className="time" onClick={time1}>
-          15:45
-          <br />-<br />
-          16:30
-        </div>
-      </div>
-      <div className="title"> Available Rooms</div>
-      <div className="romsdel">
-        {rooms.map((data) => (
-          <h4>{data}</h4>
+        {btnDiv1.map((buttonText, index) => (
+          <div className="time" onClick={() => getTime(index)} key={index}>
+            {buttonText}
+          </div>
         ))}
       </div>
+      <div className="slotDiv2">
+        {btnDiv2.map((buttonText, index) => (
+          <div className="time" onClick={() => getTime(index + 4)} key={index}>
+            {buttonText}
+          </div>
+        ))}
+      </div>
+      <div className="slotDiv3">
+        {btnDiv3.map((buttonText, index) => (
+          <div className="time" onClick={() => getTime(index + 8)} key={index}>
+            {buttonText}
+          </div>
+        ))}
+      </div>
+      <div className="title"> Available Rooms</div>
+      <div className="romsdel">data</div>
     </div>
   );
 }
