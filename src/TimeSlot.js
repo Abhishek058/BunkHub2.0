62fde80e9947d1a620cd0e3ed85b0ab4bf6e9d6f
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-18 16:04:45 +0530

    data displaying on web correctly | and api fixed

--- src/TimeSlot.js
+++ src/TimeSlot.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./time.json";
 
 const btnDiv1 = ["9:00-9:45", "9:45-10:30", "10:30-11:15", "11:15-12:00"];
 const btnDiv2 = ["12:00-9:45", "12:45-13:30", "13:30-14:15", "14:15-15:00"];
 const btnDiv3 = ["15:00-15:45", "15:45-16:30"];
 
-const getTime = async (n) => {
-  console.log(data.data[n].avlRooms);
-};
-
 function TimeSlot() {
+  const [students, setStudents] = useState("");
+  const getTime = async (n) => {
+    const roomData = data.data[n].avlRooms;
+    setStudents(roomData);
+    console.log(roomData);
+  };
   return (
     <div className="timeSlot">
       <div className="slotDiv1">
@@ -37,7 +39,7 @@ function TimeSlot() {
       <br></br>
       <div className="title"> Available Rooms</div>
       <div className="romsdel">
-        <h4>data</h4>
+        <h3>{students}</h3>
       </div>
     </div>
   );
