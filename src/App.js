f82cee938e408bb7c910fb9fbf1ccb31b8ce75d8
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-01-19 22:47:44 +0530

    Download section created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header.js";
 import TimeSlot from "./TimeSlot";
+import Download from "./Download";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TimeSlot />
+      <Download />
     </div>
   );
 }
---
+++ src/Download.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Download() {
+  return (
+    <div className="download">
+      <a href="https://drive.google.com/file/d/1EZRn9H1xLXrEtnlpJ-I-xjm9yIqX8iK-/view?usp=share_link">
+        👉 CSE 1 Time TimeTable
+      </a>
+      <a href="https://drive.google.com/file/d/12TTiQ3RjeZVoYpEBoSIIUx0QU5OHR_sa/view?usp=share_link">
+        👉 CSE 2 Time TimeTable
+      </a>
+    </div>
+  );
+}
+
+export default Download;
